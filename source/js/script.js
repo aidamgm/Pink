1913c0fabede16323e7400c6d0955dbf823195ac
Аида Магомаева <aida.magomaeva@mail.ru>
2022-12-04 18:07:22 +0300

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -37,3 +37,22 @@ likes.addEventListener("click", function () {
   }
 });
 
+const input = document.querySelector('input');
+input.addEventListener('input', onInput);
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.borderColor = 'green';
+  } else {
+    input.style.borderColor = 'red';
+  }
+}
+
+input.addEventListener('input', onInput);
+
+function isEmailValid(value) {
+return EMAIL_REGEXP.test(value);
+};
