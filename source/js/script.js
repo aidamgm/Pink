ebbfa0dbea735b66ca32766b6e1004eba07b7cf6
Аида Магомаева <aida.magomaeva@mail.ru>
2022-12-04 18:17:30 +0300

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -56,3 +56,13 @@ input.addEventListener('input', onInput);
 function isEmailValid(value) {
 return EMAIL_REGEXP.test(value);
 };
+
+function ValidPhone() {
+  var re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
+  var myPhone = document.getElementById('phone-number').value;
+  var valid = re.test(myPhone);
+  if (valid) output = 'Номер телефона введен правильно!';
+  else output = 'Номер телефона введен неправильно!';
+  document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+  return valid;
+}
